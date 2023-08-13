4df9c194cb498679e732ae55576a04b7d7e2d2c4
Alves-Jose <josemarcos8133@gmail.com>
2023-08-13 14:46:37 -0300

    2nd commit

---
+++ application/controllers/Games.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Games extends CI_Controller {
+	
+	public function index()
+	{
+		$this->load->model('games_model');
+		$data['games'] = $this->games_model->index();
+        $data['title']  = 'Games - CodeIgniter';
+		
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/nav-top', $data);
+        $this->load->view('templates/footer', $data);
+        $this->load->view('templates/js', $data);
+        $this->load->view('pages/games', $data);
+        
+
+	}
+
+    public function new()
+    {
+        $data['title']  = 'Games - CodeIgniter';
+        
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/nav-top', $data);
+        $this->load->view('templates/footer', $data);
+        $this->load->view('templates/js', $data);
+        $this->load->view('pages/form-games', $data);
+    }
+}
